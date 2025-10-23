9b2b966ec3be5f3c194b844dc488108007353bd3
Nehir <250541039@firat.edu.tr>
2025-10-23 15:13:11 +0300

    Update BesKereTekrar.java

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,9 @@
+public class Yazi {
+    public static void main(String[] args) {
+        // Görseldeki metin satır satır yazdırılıyor
+        System.out.println("J A V V A");
+        System.out.println("J A A V V A A");
+        System.out.println("J J AAAAA V V AAAAA");
+        System.out.println("J J A A V A A");
+    }
+}
