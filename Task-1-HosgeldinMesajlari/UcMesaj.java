dd08537c257ddbee065bd0b8a10e34e8127b71ff
Nehir <250541039@firat.edu.tr>
2025-10-23 15:19:16 +0300

    Update UcMesaj.java

---
+++ Task-1-HosgeldinMesajlari/UcMesaj.java
@@ -0,0 +1,10 @@
+public class Gorev1_1 {
+
+    public static void main(String[] args) {
+        
+        // Görev 1.1: Ekrana üç farklı mesaj yazdırır.
+        System.out.println("Java'ya Hoş Geldiniz");
+        System.out.println("Bilgisayar Bilimine Hoş Geldiniz");
+        System.out.println("Programlama Eğlencelidir");
+    }
+}
